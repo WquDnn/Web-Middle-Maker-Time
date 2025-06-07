949d0859378892779b662f1b7619a064235eb718
Deanny-MrSuicide <yarynkagordynska2000@gmail.com>
2025-06-07 17:36:13 +0300

    коміт

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('http://api.weatherapi.com/v1/current.json?key=5d8b0b5d6fb24500869132736253105&q=London&aqi=no')
+fetch('http://api.weatherapi.com/v1/forecast.json?key=5d8b0b5d6fb24500869132736253105&q=London&aqi=no&days=3')
 .then(res => res.json())
 .then(data => {
     console.log(data)
@@ -7,16 +7,16 @@ fetch('http://api.weatherapi.com/v1/current.json?key=5d8b0b5d6fb2450086913273625
      let humidity = document.querySelector(".humidity").innerHTML = data.current.humidity
      let cloud = document.querySelector(".cloud").innerHTML = data.current.cloud
      let pres = document.querySelector(".precip_in").innerHTML = data.current.precip_in
-    
-     if (humidity <= 40 && cloud <= 70 && pres == 0 && temp >= 30) {
+     let feelslike = document.querySelector(".feelslike_c").innerHTML = data.current.feelslike_c;
 
-        console.log("Сонячно")
-     }else if (cloud >= 70 && humidity >= 40 && pres <= 20 ) {
-        console.log("хмарно")
-     }else {
-        console.log("da ya")
+
+      console.log(feelslike)
+  
      }
 
+
+
+
      
-})
+   )
 

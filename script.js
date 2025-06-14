b48404be71a6e6e91a537462de74fabeb6967c17
Teremki16 <teremki16robocode@gmail.com>
2025-06-14 16:37:33 +0300

    input

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let city = "Kyiv"
+
 function getWeather(city, day){
 
 fetch(`http://api.weatherapi.com/v1/forecast.json?key=5d8b0b5d6fb24500869132736253105&q=${city}&aqi=no&days=3`)
@@ -6,37 +8,37 @@ fetch(`http://api.weatherapi.com/v1/forecast.json?key=5d8b0b5d6fb245008691327362
       console.log(data)
       document.querySelector(".weat").innerHTML = "Weather today - " + data.location.name
       document.querySelector(".city").innerHTML = data.location.name
-      let avgtemp_c = document.querySelector(".avgtemp_c").innerHTML = data.forecast.forecastday[day].day.avgtemp_c + "℃"
-      let avghumidity = document.querySelector(".avghumidity").innerHTML = data.forecast.forecastday[day].day.avghumidity + "%"
-      let maxwind_mph = document.querySelector(".maxwind_mph").innerHTML = data.forecast.forecastday[day].day.maxwind_mph + "%"
+      // let avgtemp_c = document.querySelector(".avgtemp_c").innerHTML = data.forecast.forecastday[day].day.avgtemp_c + "℃"
+      // let avghumidity = document.querySelector(".avghumidity").innerHTML = data.forecast.forecastday[day].day.avghumidity + "%"
+      // let maxwind_mph = document.querySelector(".maxwind_mph").innerHTML = data.forecast.forecastday[day].day.maxwind_mph + "%"
       // let pres = document.querySelector(".precip_in").innerHTML = data.current.precip_in + "%"
-      let maxtemp_c = document.querySelector(".maxtemp_c").innerHTML = data.forecast.forecastday[day].day.maxtemp_c + "℃"
+      // let maxtemp_c = document.querySelector(".maxtemp_c").innerHTML = data.forecast.forecastday[day].day.maxtemp_c + "℃"
       
       document.querySelector(".stan").innerHTML = data.current.condition.text + `<img src="${data.current.condition.icon}">`
 
-      if (humidity <= 40 && cloud <= 70 && pres == 0 && temp >= 30) {
-         document.body.style.background = "linear-gradient(rgb(151, 234, 234) 60%, rgba(240, 239, 239, 0.975))"
-         document.querySelector(".box").style.background = "url(https://cikavosti.com/wp-content/uploads/2015/01/24-01-15-foto-600x400-11.jpg)"
-         console.log("Сонячно")
-      } else if (cloud >= 70 && humidity >= 40 && pres <= 20) {
+      // if (humidity <= 40 && cloud <= 70 && pres == 0 && temp >= 30) {
+      //    document.body.style.background = "linear-gradient(rgb(151, 234, 234) 60%, rgba(240, 239, 239, 0.975))"
+      //    document.querySelector(".box").style.background = "url(https://cikavosti.com/wp-content/uploads/2015/01/24-01-15-foto-600x400-11.jpg)"
+      //    console.log("Сонячно")
+      // } else if (cloud >= 70 && humidity >= 40 && pres <= 20) {
         
-         document.body.style.background = "linear-gradient(rgb(110, 157, 157) 60%, rgba(240, 239, 239, 0.975))"
-         document.querySelector(".box").style.background = "url(https://media.acc.cv.ua/news/article/2019/02/02/41015/xwBR5Yr1tYrfgygXB3mA.w575.jpg)"
-         console.log("Хмарно")
-      } else {
+      //    document.body.style.background = "linear-gradient(rgb(110, 157, 157) 60%, rgba(240, 239, 239, 0.975))"
+      //    document.querySelector(".box").style.background = "url(https://media.acc.cv.ua/news/article/2019/02/02/41015/xwBR5Yr1tYrfgygXB3mA.w575.jpg)"
+      //    console.log("Хмарно")
+      // } else {
         
-         document.body.style.background = "linear-gradient(rgb(176, 228, 228) 60%, rgba(240, 239, 239, 0.975))"
-         document.querySelector(".box").style.background = "url(https://konkurent.ua/media/cache/71/95/71956ce3219d48d638ca06a70004d460.webp)"
-         console.log("Мінлива хмарність")
-      }
+      //    document.body.style.background = "linear-gradient(rgb(176, 228, 228) 60%, rgba(240, 239, 239, 0.975))"
+      //    document.querySelector(".box").style.background = "url(https://konkurent.ua/media/cache/71/95/71956ce3219d48d638ca06a70004d460.webp)"
+      //    console.log("Мінлива хмарність")
+      // }
 
 
    })
 }
 
-getWeather("Kyiv", 0)
+getWeather(city, 0)
 
-document.querySelector(".Tommorow").addEventListener("click", function () {
+document.querySelector(".Tomorrow").addEventListener("click", function () {
      getWeather("Kyiv", 1)
 })
 document.querySelector("#input").addEventListener("input", (e)=>{
@@ -46,5 +48,8 @@ document.querySelector("#input").addEventListener("input", (e)=>{
 })
 
 document.querySelector("#search").addEventListener("click", ()=>{
-   
+   if(){
+      city = document.querySelector("#input").value
+      getWeather(city, 0)
+   }
 })

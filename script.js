9bf67210b3dadc1e2dea63a0d0bb0921d2087bbb
Deanny-MrSuicide <yarynkagordynska2000@gmail.com>
2025-06-14 16:37:10 +0300

    gays2

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ fetch(`http://api.weatherapi.com/v1/forecast.json?key=5d8b0b5d6fb245008691327362
       console.log(data)
       document.querySelector(".weat").innerHTML = "Weather today - " + data.location.name
       document.querySelector(".city").innerHTML = data.location.name
-      let avgtemp_c = document.querySelector(".avgtemp_c").innerHTML = data.forecast.forecastday[day].day.avgtemp_c + "℃"
-      let avghumidity = document.querySelector(".avghumidity").innerHTML = data.forecast.forecastday[day].day.avghumidity + "%"
+      let temp = document.querySelector(".temp").innerHTML = data.forecast.forecastday[day].day.avgtemp_c + "℃"
+      let humidity = document.querySelector(".humidity").innerHTML = data.forecast.forecastday[day].day.avghumidity + "%"
       let maxwind_mph = document.querySelector(".maxwind_mph").innerHTML = data.forecast.forecastday[day].day.maxwind_mph + "%"
-      // let pres = document.querySelector(".precip_in").innerHTML = data.current.precip_in + "%"
+      let pres = document.querySelector(".precip_in").innerHTML = data.forecast.forecastday[1].day.totalprecip_in + "%"
       let maxtemp_c = document.querySelector(".maxtemp_c").innerHTML = data.forecast.forecastday[day].day.maxtemp_c + "℃"
       
       document.querySelector(".stan").innerHTML = data.current.condition.text + `<img src="${data.current.condition.icon}">`
 
-      if (humidity <= 40 && cloud <= 70 && pres == 0 && temp >= 30) {
+      if (humidity <= 40 && pres == 0 && temp >= 30) {
          document.body.style.background = "linear-gradient(rgb(151, 234, 234) 60%, rgba(240, 239, 239, 0.975))"
          document.querySelector(".box").style.background = "url(https://cikavosti.com/wp-content/uploads/2015/01/24-01-15-foto-600x400-11.jpg)"
          console.log("Сонячно")
-      } else if (cloud >= 70 && humidity >= 40 && pres <= 20) {
+      } else if ( humidity >= 40 && pres <= 20) {
         
          document.body.style.background = "linear-gradient(rgb(110, 157, 157) 60%, rgba(240, 239, 239, 0.975))"
          document.querySelector(".box").style.background = "url(https://media.acc.cv.ua/news/article/2019/02/02/41015/xwBR5Yr1tYrfgygXB3mA.w575.jpg)"
@@ -36,9 +36,11 @@ fetch(`http://api.weatherapi.com/v1/forecast.json?key=5d8b0b5d6fb245008691327362
 
 getWeather("Kyiv", 0)
 
-document.querySelector(".Tommorow").addEventListener("click", function () {
+document.querySelector(".Tomorrow").addEventListener("click", function () {
      getWeather("Kyiv", 1)
 })
+
+
 document.querySelector("#input").addEventListener("input", (e)=>{
    fetch(`http://api.weatherapi.com/v1/search.json?key=5d8b0b5d6fb24500869132736253105&q=${e.target.value}`).then(res => res.json()).then(data => {
       document.querySelector("#list").innerHTML = data.map(city => `<option value="${city.name}"></option>`).join("")

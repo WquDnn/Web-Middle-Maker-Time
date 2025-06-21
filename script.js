dc7ab14da7e03c503c50d3e49450b1b041c784b7
Teremki16 <teremki16robocode@gmail.com>
2025-06-21 17:31:58 +0300

    Patchy rain or Light rain

--- script.js
+++ script.js
@@ -14,59 +14,75 @@ function getWeather(city, day) {
          document.querySelector(".city").innerHTML = data.location.name
          let temp = document.querySelector(".temp").innerHTML = data.forecast.forecastday[day].day.avgtemp_c + "℃"
          let humidity = document.querySelector(".humidity").innerHTML = data.forecast.forecastday[day].day.avghumidity + "%"
-         let maxwind_mph = document.querySelector(".maxwind_mph").innerHTML = data.forecast.forecastday[day].day.maxwind_mph + "%"
+         let maxwind_mph = document.querySelector(".maxwind_mph").innerHTML = data.forecast.forecastday[day].day.maxwind_mph + "km/h"
          let pres = document.querySelector(".precip_in").innerHTML = data.forecast.forecastday[day].day.totalprecip_in + "%"
          let maxtemp_c = document.querySelector(".maxtemp_c").innerHTML = data.forecast.forecastday[day].day.maxtemp_c + "℃"
          let mintemp_c = document.querySelector(".mintemp_c").innerHTML = data.forecast.forecastday[day].day.mintemp_c + "℃"
          let chanceofrain = document.querySelector(".chanceofrain").innerHTML = data.forecast.forecastday[day].day.daily_chance_of_rain + "%"
          let feelsLike1 = document.querySelector(".feelsLike").innerHTML = feelsLike(data.forecast.forecastday[day].day.avgtemp_c, data.forecast.forecastday[day].day.avghumidity / 100) + "℃"
-
-
-
-
-
+         
+         
+         
+         chanceofrain = data.forecast.forecastday[day].day.daily_chance_of_rain 
+         console.log(chanceofrain)
 
          document.querySelector(".stan").innerHTML = data.current.condition.text + `<img src="${data.current.condition.icon}">`
+         let tan = data.current.condition.text
+         // if (chanceofrain < 11) {
+         //    document.body.style.background = "linear-gradient(rgb(151, 234, 234) 60%, rgba(240, 239, 239, 0.975))"
+         //    document.querySelector(".box").style.background = "url(https://cikavosti.com/wp-content/uploads/2015/01/24-01-15-foto-600x400-11.jpg)"
+         //    console.log("Сонячно")
+         // } else if (chanceofrain > 49) {
+         //    document.body.style.background = "linear-gradient(rgb(110, 157, 157) 60%, rgba(240, 239, 239, 0.975))"
+         //    document.querySelector(".box").style.background = "url(https://media.acc.cv.ua/news/article/2019/02/02/41015/xwBR5Yr1tYrfgygXB3mA.w575.jpg)"
+         //    console.log("Хмарно")
+         // } else {
 
-         if (humidity <= 40 && pres == 0 && temp >= 30) {
+         //    document.body.style.background = "linear-gradient(rgb(176, 228, 228) 60%, rgba(240, 239, 239, 0.975))"
+         //    document.querySelector(".box").style.background = "url(https://konkurent.ua/media/cache/71/95/71956ce3219d48d638ca06a70004d460.webp)"
+         //    console.log("Мінлива хмарність")
+         // }
+
+         if (tan == "Sunny") {
             document.body.style.background = "linear-gradient(rgb(151, 234, 234) 60%, rgba(240, 239, 239, 0.975))"
             document.querySelector(".box").style.background = "url(https://cikavosti.com/wp-content/uploads/2015/01/24-01-15-foto-600x400-11.jpg)"
             console.log("Сонячно")
-         } else if (humidity >= 40 && pres <= 20) {
-            // if (humidity <= 40 && cloud <= 70 && pres == 0 && temp >= 30) {
-            //    document.body.style.background = "linear-gradient(rgb(151, 234, 234) 60%, rgba(240, 239, 239, 0.975))"
-            //    document.querySelector(".box").style.background = "url(https://cikavosti.com/wp-content/uploads/2015/01/24-01-15-foto-600x400-11.jpg)"
-            //    console.log("Сонячно")
-            // } else if (cloud >= 70 && humidity >= 40 && pres <= 20) {
+         } else if (tan == "Patchy rain nearby") {
+            document.body.style.background = "linear-gradient(rgb(110, 157, 157) 60%, rgba(240, 239, 239, 0.975))"
+            document.querySelector(".box").style.background = "url(https://media.acc.cv.ua/news/article/2019/02/02/41015/xwBR5Yr1tYrfgygXB3mA.w575.jpg)"
+            console.log("Хмарно")
+         } else if (tan == "Partly cloudy") {
+            document.body.style.background = "linear-gradient(rgb(162, 215, 215) 60%, rgba(240, 239, 239, 0.975))"
+            document.querySelector(".box").style.background = "url(https://wpcdn.us-east-1.vip.tn-cloud.net/www.klkntv.com/content/uploads/2020/04/cloud1.jpg)"
+            console.log("Хмарно")
+         } else if (tan == "Cloudy") {
+            document.body.style.background = "linear-gradient(rgb(168, 204, 204) 60%, rgba(240, 239, 239, 0.975))"
+            document.querySelector(".box").style.background = "url(https://t3.ftcdn.net/jpg/03/02/03/70/360_F_302037028_WgdzBqp7MCTF0iITajUUVryCKJsyjOE6.jpg)"
+            console.log("Хмарно")
+         } else if(tan == "Light rain shower"){
+            document.body.style.background = "linear-gradient(rgb(176, 228, 228) 60%, rgba(240, 239, 239, 0.975))"
+            document.querySelector(".box").style.background = "url(https://www.farnhamherald.com/tindle-static/image/2025/03/09/17/49/Light-Rain-2.jpeg?trim=0,0,0,0&width=752&height=500&crop=752:500)"
+            console.log("Мінлива хмарність")
+         }
 
-               document.body.style.background = "linear-gradient(rgb(110, 157, 157) 60%, rgba(240, 239, 239, 0.975))"
-               document.querySelector(".box").style.background = "url(https://media.acc.cv.ua/news/article/2019/02/02/41015/xwBR5Yr1tYrfgygXB3mA.w575.jpg)"
-               console.log("Хмарно")
-            } else {
-
-               document.body.style.background = "linear-gradient(rgb(176, 228, 228) 60%, rgba(240, 239, 239, 0.975))"
-               document.querySelector(".box").style.background = "url(https://konkurent.ua/media/cache/71/95/71956ce3219d48d638ca06a70004d460.webp)"
-               console.log("Мінлива хмарність")
-            }
 
 
-
-         })
+      })
 }
 
 
 getWeather(city, 0)
 
 document.querySelector(".Tomorrow").addEventListener("click", function () {
-   getWeather("Kyiv", 1)
+   getWeather(city, 1)
 })
 
 document.querySelector(".DAT").addEventListener("click", function () {
-   getWeather("Kyiv", 2)
+   getWeather(city, 2)
 })
 
 document.querySelector(".Today").addEventListener("click", function () {
-   getWeather("Kyiv", 0)
+   getWeather(city, 0)
 })
 
 
